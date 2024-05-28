0ff6839a1cc2452bdcd6100fbf0b0b357e852f1c
Abdul Rahman <ar69king@gmail.com>
2024-05-28 20:46:20 +0500

    new

--- api/views.py
+++ api/views.py
@@ -2,10 +2,14 @@ from django.http import JsonResponse
 from api.authenticate import authenticate
 from api.classroom_api import notify_new_activity
 from googleapiclient.discovery import build
+import traceback
 
 service = build("classroom", "v1", credentials=authenticate())
 
 def get(request):
     if request.method == "GET":
-        notify_new_activity(service)
-        return JsonResponse({"message": "Notification sent successfully."})
+        try:
+            notify_new_activity(service)
+            return JsonResponse({"message": "Notification sent successfully."})
+        except Exception as e:
+            return JsonResponse({"message": f"An error occurred\n{traceback.format_exc()}"}, status=500)
