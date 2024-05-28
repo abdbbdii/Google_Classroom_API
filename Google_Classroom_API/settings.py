104be5cff332781dda289ec0f05e6d267ff73169
Abdul Rahman <ar69king@gmail.com>
2024-05-28 21:10:00 +0500

    .

--- Google_Classroom_API/settings.py
+++ Google_Classroom_API/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 import os
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- api/views.py
+++ api/views.py
@@ -6,9 +6,10 @@ from dotenv import find_dotenv
 
 service = build("classroom", "v1", credentials=authenticate())
 
+
 def get(request):
     if request.method == "GET":
-        return JsonResponse({"path":find_dotenv()})
+        return JsonResponse({"path": find_dotenv()})
         # try:
         #     notify_new_activity(service)
         #     return JsonResponse({"message": "Notification sent successfully."})
