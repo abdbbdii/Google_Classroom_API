f5dcd4317cd926c66ad4d30a1ce686c13217b1ea
Abdul Rahman <ar69king@gmail.com>
2024-09-06 18:11:05 +0500

    patch

--- api/classroom_api.py
+++ api/classroom_api.py
@@ -10,11 +10,10 @@ def parse_datetime(dt_str):
         return datetime.strptime(dt_str, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc)
 
 
-def get_new_item(service, course, item_type):
+def get_new_item(service, course, item_type, last_check):
     items = service.courses().courseWorkMaterials().list(courseId=course["id"]).execute()
     for item in items.get(item_type, []):
-        item_time = parse_datetime(item["updateTime"])
-        if item_time > appSettings.last_check:
+        if parse_datetime(item["updateTime"]) > last_check:
             print(f"New {item} found")
             # profile = service.userProfiles().get(userId=item["ownerId"]).execute()
             # owner_name = profile.get("name", {}).get("fullName")
@@ -28,12 +27,12 @@ def get_new_item(service, course, item_type):
 
 
 def notify_new_activity(service):
-    appSettings.last_check = datetime.fromisoformat(appSettings.last_check).replace(tzinfo=timezone.utc) if appSettings.last_check is not None else datetime.now(timezone.utc)
+    last_check = datetime.fromisoformat(appSettings.last_check).replace(tzinfo=timezone.utc) if appSettings.last_check is not None else datetime.now(timezone.utc)
     appSettings.update("last_check", datetime.now(timezone.utc).isoformat())
 
     courses = service.courses().list().execute().get("courses", [])
     for course in courses:
         print(f"Checking for new activity in course {course['name']}...")
-        get_new_item(service, course, "announcements")
-        get_new_item(service, course, "courseWork")
-        get_new_item(service, course, "courseWorkMaterial")
+        get_new_item(service, course, "announcements", last_check)
+        get_new_item(service, course, "courseWork", last_check)
+        get_new_item(service, course, "courseWorkMaterial", last_check)
