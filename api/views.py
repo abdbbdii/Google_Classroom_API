ee60508bad5ab0965683bcba6e63658d41547207
Abdul Rahman <ar69king@gmail.com>
2024-09-26 14:06:33 +0500

    test

--- api/views.py
+++ api/views.py
@@ -12,28 +12,32 @@ from .appSettings import appSettings
 
 def get(request):
     try:
-        r = requests.get(
-            appSettings.utils_server_url + "service/google_auth/",
-            json={
-                "password": appSettings.utils_server_password,
-                "token_pickle_base64": appSettings.token_pickle_base64,
-                "scopes": [
-                    "https://www.googleapis.com/auth/classroom.courses.readonly",
-                    "https://www.googleapis.com/auth/classroom.student-submissions.me.readonly",
-                    "https://www.googleapis.com/auth/classroom.announcements.readonly",
-                    "https://www.googleapis.com/auth/classroom.courseworkmaterials.readonly",
-                ],
-            },
-        )
+        try:
+            service = build("classroom", "v1", credentials=pickle.loads(base64.b64decode(appSettings.token_pickle_base64)))
+            notify_new_activity(service)
+        except:
+            r = requests.get(
+                appSettings.utils_server_url + "service/google_auth/",
+                json={
+                    "password": appSettings.utils_server_password,
+                    "token_pickle_base64": appSettings.token_pickle_base64,
+                    "scopes": [
+                        "https://www.googleapis.com/auth/classroom.courses.readonly",
+                        "https://www.googleapis.com/auth/classroom.student-submissions.me.readonly",
+                        "https://www.googleapis.com/auth/classroom.announcements.readonly",
+                        "https://www.googleapis.com/auth/classroom.courseworkmaterials.readonly",
+                    ],
+                },
+            )
 
-        if r.status_code != 200:
-            return JsonResponse({"error": r.json().get("message")}, status=500)
+            print(r.json())
 
-        if r.json().get("token_pickle_base64") != appSettings.token_pickle_base64:
-            appSettings.update("token_pickle_base64", r.json().get("token_pickle_base64"))
+            if r.status_code != 200:
+                return JsonResponse({"error": r.json().get("message")}, status=500)
 
-        service = build("classroom", "v1", credentials=pickle.loads(base64.b64decode(appSettings.token_pickle_base64)))
-        notify_new_activity(service)
+            if r.json().get("token_pickle_base64") != appSettings.token_pickle_base64:
+                appSettings.update("token_pickle_base64", r.json().get("token_pickle_base64"))
+
 
         return JsonResponse({"message": "Notification sent successfully."})
 
