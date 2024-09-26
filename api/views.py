3ebc89fbf65eaeedd816f0b45e081ac7828fec83
Abdul Rahman <ar69king@gmail.com>
2024-09-26 20:11:10 +0500

    test

--- api/views.py
+++ api/views.py
@@ -16,29 +16,30 @@ def get(request):
         try:
             service = build("classroom", "v1", credentials=pickle.loads(base64.b64decode(appSettings.token_pickle_base64)))
             async_to_sync(notify_new_activity)(service)
-        except:
-            r = requests.get(
-                appSettings.utils_server_url + "service/google_auth/",
-                json={
-                    "password": appSettings.utils_server_password,
-                    "token_pickle_base64": appSettings.token_pickle_base64,
-                    "scopes": [
-                        "https://www.googleapis.com/auth/classroom.courses.readonly",
-                        "https://www.googleapis.com/auth/classroom.student-submissions.me.readonly",
-                        "https://www.googleapis.com/auth/classroom.announcements.readonly",
-                        "https://www.googleapis.com/auth/classroom.courseworkmaterials.readonly",
-                    ],
-                },
-            )
+        except Exception as e:
+            # r = requests.get(
+            #     appSettings.utils_server_url + "service/google_auth/",
+            #     json={
+            #         "password": appSettings.utils_server_password,
+            #         "token_pickle_base64": appSettings.token_pickle_base64,
+            #         "scopes": [
+            #             "https://www.googleapis.com/auth/classroom.courses.readonly",
+            #             "https://www.googleapis.com/auth/classroom.student-submissions.me.readonly",
+            #             "https://www.googleapis.com/auth/classroom.announcements.readonly",
+            #             "https://www.googleapis.com/auth/classroom.courseworkmaterials.readonly",
+            #         ],
+            #     },
+            # )
 
-            print(r.json())
+            # print(r.json())
 
-            if r.status_code != 200:
-                return JsonResponse({"error": r.json().get("message")}, status=500)
+            # if r.status_code != 200:
+            #     return JsonResponse({"error": r.json().get("message")}, status=500)
 
-            if r.json().get("token_pickle_base64") != appSettings.token_pickle_base64:
-                appSettings.update("token_pickle_base64", r.json().get("token_pickle_base64"))
+            # if r.json().get("token_pickle_base64") != appSettings.token_pickle_base64:
+            #     appSettings.update("token_pickle_base64", r.json().get("token_pickle_base64"))
 
+            return JsonResponse({"message": str(traceback.format_exc())})
 
         return JsonResponse({"message": "Notification sent successfully."})
 
